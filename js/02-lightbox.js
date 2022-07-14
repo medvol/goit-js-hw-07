3b044a9b777c26f451d9478e21842d836407c539
Oleh Medvid <olmedvid@gmail.com>
2022-07-14 15:13:31 +0300

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const createItemMarkup = (items) => {
+      
+    return items.reduce((acc, item) => {
+        const { preview, original, description } = item;
+
+        return acc + `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>`
+
+    }, '')
+     
+}
+
+const containerImage = document.querySelector('.gallery')
+
+const listImage = createItemMarkup(galleryItems);
+const addImageMarkup = containerImage.insertAdjacentHTML('beforeend', listImage);
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+});
